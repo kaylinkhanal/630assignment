16b4baa53537ae121b080d8b9feb90e79f4bed79
Kaylin Khanal <kyalinnop@gmail.com>
2024-08-04 07:02:23 +0545

    added arr level 4 question

--- javascript/array/level4.js
+++ javascript/array/level4.js
@@ -5,7 +5,17 @@ const arr = ['ram','shyam','hari']
 // expected output 
 // ['ram', 'hari']
 //syntax
-//const output = arr.filter(()=>{
+//const output = arr.filter((item)=>{
 
+// })
+//console.log(output)
+
+
+
+//Q2 count total strings which have length 4
+//expected output : 1
+// let total = 0
+//arr.forEach((item,id)=>{
+    // // yedi item.length === 4, total ma add 1
 // })
 //console.log(output)
