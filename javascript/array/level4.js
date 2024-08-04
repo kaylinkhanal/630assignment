fbaf5911e8c6bcb2320848e92978fab53373d65c
Kaylin Khanal <kyalinnop@gmail.com>
2024-08-04 07:21:42 +0545

    added arr level 4 question

--- javascript/array/level4.js
+++ javascript/array/level4.js
@@ -14,18 +14,29 @@ console.log(output)
 
 //Q2 count total strings which have length 4
 //expected output : 1
-// let total = 0
-//arr.forEach((item,id)=>{
-    // // yedi item.length === 4, total ma add 1
-// })
-//console.log(output)
+let total = 0
+arr.forEach((item,id)=>{
+    // yedi item.length === 4, total ma add 1
+    if(item.length ===4) {
+        total= total + 1
+    }
+})
+console.log(total)
 
 // high level
 // Q3 
 // expected output: 12
 // count total characters in all the strings in the array
-// let strCount = 0
-//arr.forEach((item,id)=>{
-    // // add item.length  to strCount
-// })
-//console.log(strCount)
+let strCount = 0
+arr.forEach((item,id)=>{
+   strCount = strCount + item.length
+})
+console.log(strCount)
+
+
+// Q4 using map, get the last characters
+const lastOutput = arr.map((item)=>{
+    
+  })
+  console.log(lastOutput)
+//['m','m','i']
