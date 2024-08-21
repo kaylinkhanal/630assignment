4cb8fd2551528bcd9f63e79686617b4edd2d84f4
Kaylin Khanal <kyalinnop@gmail.com>
2024-08-21 08:06:02 +0545

    add bird game

--- react/nextjs/src/app/(auth)/rockpaper/page.js
+++ react/nextjs/src/app/(auth)/rockpaper/page.js
@@ -1,33 +1,41 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaRegHandRock } from "react-icons/fa";
 import { FaRegHandPaper } from "react-icons/fa";
 import { FaRegHandScissors } from "react-icons/fa";
 
 import Confetti from 'react-confetti'
+import { Button } from '@nextui-org/react';
 
 const RockPaper = () => {
     const [userChoice, setUserChoice] = useState(null)
+    const [newRound, setNewRound] = useState(1)
+    const [randomNum ,setRandomNum] = useState(0)
     const handleChange = (choice)=>{
         setUserChoice(choice)
+        setNewRound(newRound+1)
     }
     const choices = [
         {name:'rock', icon:  <FaRegHandRock size={100} onClick={()=> handleChange('rock')}/> },
         {name:'paper', icon:  <FaRegHandPaper size={100} onClick={()=> handleChange('paper')}/> },
          {name:'scissor', icon:  <FaRegHandScissors size={100} onClick={()=> handleChange('scissor')}/> },
     ]
-    const randomNum = Math.floor(Math.random() * choices.length)
+    useEffect(()=>{
+        const newNum = Math.floor(Math.random() * choices.length)
+        setRandomNum(newNum)
+    },[userChoice, newRound])
     const confetti =  <Confetti
     width={1300}
     height={300}
   />
   return ( 
     <div>
+        Round Number: {newRound}
         <div className='flex gap-6  m-4 p-4 items-center'>
             <div className='bg-pink-100 shadow-lg  w-[50%] h-72'>Computer
             { userChoice && choices[randomNum].icon}
             { userChoice && choices[randomNum].name}
-
+                <Button onClick={()=>setUserChoice(null)}>Reset</Button>
             </div>
             <div className='flex gap-4 justify-center items-center bg-green-100 shadow-lg 
              rounded-xl w-[50%] h-72'>
